30bb7bae82811855e0a0db84608b89a5e3137d62
ab199000 <97779753+ab199000@users.noreply.github.com>
2023-12-18 23:24:13 +0300

    Add files via upload

--- src/app/App.js
+++ src/app/App.js
@@ -1,29 +1,23 @@
 import React from "react";
-import Header from "../Components/Header/Header";
-import styles from "./App.module.css"
-import Footer from "../Components/Footer/Footer";
-import BookCard from "../Components/BookCard/BookCard";
-import Slider from "../Components/Slider/Slider";
-import Map from "../Components/Map/Map";
-import OurShops from "../pages/OurShops/OuerShops";
-import AboutUs from "../pages/AboutUs/AboutUs";
-import BasketItem from "../Components/BasketItem/BasketItem";
-import Basket from "../pages/Basket/Basket";
-import BookInforCard from "../Components/BookIforCard/BookInforCard";
+// import Header from "../Components/Header/Header";
+// import styles from "./App.module.css"
+// import Footer from "../Components/Footer/Footer";
+// import BookCard from "../Components/BookCard/BookCard";
+// import Slider from "../Components/Slider/Slider";
+// import Map from "../Components/Map/Map";
+// import OurShops from "../pages/OurShops/OuerShops";
+// import AboutUs from "../pages/AboutUs/AboutUs";
+// import BasketItem from "../Components/BasketItem/BasketItem";
+// import Basket from "../pages/Basket/Basket";
+// import BookInforCard from "../Components/BookIforCard/BookInforCard";
+import BookPage from "../pages/BookPage/BookPage";
 
 function App() {
-  return (
-    <div className="App">
-      {/* <Header/>
-      <Slider/> */}
-      {/* <OurShops/> */}
-      {/* <BasketItem/> */}
-      {/* <Footer/> */}
-      {/* <AboutUs/> */}
-      <BookInforCard/>
-      {/* <Basket/> */}
-    </div>
-  );
+    return (
+        <div className="App">
+            <BookPage/>
+        </div>
+    );
 }
 
 export default App;
